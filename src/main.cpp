305a4be1e01b0a50d9434781934b2d018c368b87
Drake Witt <dwitt@dranweb.com>
2024-10-23 17:52:55 -0400

    (wip) multi universe

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,27 @@
 #include <FastLED.h>
 #include <ETH.h>
 
-#define NUMBER_OF_LEDS 170
+#define NUM_LEDS_PER_STRIP 144 // Modulight V3 Spec
+#define NUMSTRIPS 12 // 12 per controller
+#define NB_CHANNEL_PER_LED 3 //Should be 4 if your sending tool is sending RGBW
+#define UNIVERSE_SIZE_IN_CHANNEL  (170 * 3)  //here we define a universe of 170 pixels each pixel is composed of 3 channels
+#define START_UNIVERSE 0 // Prefered for MadMapper
+
+#define NUMBER_OF_LEDS 144 * 6
 #define NB_LEDS_PER_UNIVERSE 170
 #define START_UNIVERSE 1
-#define NB_CHANNEL_PER_LED 3
-#define UNIVERSE_SIZE_IN_CHANNEL  (170 * 3)  //here we define a universe of 170 pixels each pixel is composed of 3 channels
 
 #include "artnetESP32V2.h"
 
 artnetESP32V2 artnet;
 
-CRGB *leds;
+CRGB *ledsPin14;
+CRGB *ledsPin15;
 
 // I2SClocklessLedDriver driver;
 // int pins[]={14};
 
-void artnetCallback(void *param)
+void artnetCallbackPin14(void *param)
 {
   subArtnet *subartnet = (subArtnet *)param;
   auto dataLen = subartnet->len;
@@ -25,7 +30,21 @@ void artnetCallback(void *param)
   for (int i = 0; i < dataLen; i += NB_CHANNEL_PER_LED)
   {
     size_t ledIdx = i / NB_CHANNEL_PER_LED;
-    leds[ledIdx] = CRGB(subartnet->data[i], subartnet->data[i + 1], subartnet->data[i + 2]);
+    ledsPin14[ledIdx] = CRGB(subartnet->data[i], subartnet->data[i + 1], subartnet->data[i + 2]);
+  }
+
+  FastLED.show();
+}
+
+void artnetCallbackPin15(void *param)
+{
+  subArtnet *subartnet = (subArtnet *)param;
+  auto dataLen = subartnet->len;
+
+  for (int i = 0; i < dataLen; i += NB_CHANNEL_PER_LED)
+  {
+    size_t ledIdx = i / NB_CHANNEL_PER_LED;
+    ledsPin15[ledIdx] = CRGB(subartnet->data[i], subartnet->data[i + 1], subartnet->data[i + 2]);
   }
 
   FastLED.show();
@@ -34,8 +53,10 @@ void artnetCallback(void *param)
 void setup() {
   Serial.begin(115200);
 
-  leds = new CRGB[NUMBER_OF_LEDS];
-  FastLED.addLeds<WS2812B, 14, GRB>(leds, NUMBER_OF_LEDS);
+  ledsPin14 = new CRGB[NUMBER_OF_LEDS];
+  ledsPin15 = new CRGB[NUMBER_OF_LEDS];
+  FastLED.addLeds<WS2812B, 14, GRB>(ledsPin14, NUMBER_OF_LEDS);
+  FastLED.addLeds<WS2812B, 15, GRB>(ledsPin14, NUMBER_OF_LEDS);
 
   if (!ETH.begin(1, 16, 23, 18, ETH_PHY_LAN8720))
   {
@@ -50,8 +71,18 @@ void setup() {
   ETH.config(IPAddress(192, 168, 1, 10), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
   ETH.setHostname("obelisk-control-board-1");
 
-  artnet.addSubArtnet(START_UNIVERSE, NUMBER_OF_LEDS * NB_CHANNEL_PER_LED,UNIVERSE_SIZE_IN_CHANNEL ,&artnetCallback);
-  artnet.setNodeName("Arnet Node esp32");
+  // reciever size (NUM_LEDS * 3), NUM_LEDS = 144*6
+  uint32_t data_size = UNIVERSE_SIZE_IN_CHANNEL * 6;
+
+  // Universes 1-6 go to GPIO pin 14
+  artnet.addSubArtnet(1, data_size, UNIVERSE_SIZE_IN_CHANNEL ,&artnetCallbackPin14);
+
+  // Universes 7 - 12 go to GPIO pin 15
+  artnet.addSubArtnet(7, data_size, UNIVERSE_SIZE_IN_CHANNEL ,&artnetCallbackPin15);
+
+
+
+  artnet.setNodeName("SIGMA v4.20 OBAMA SIGNATURE EDITION (brain smoothed firmware v666)");
 
   if (artnet.listen(6454)) 
   {
